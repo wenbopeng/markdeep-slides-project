c6a6862ddd4a10441b854ab5cc86f62b99ad51d9
Wenbo Peng <pengwenbogood@126.com>
2025-12-12 09:47:08 +0800

    功能优化：新增一个@slides-init.js 文件，实现Markdown文档底部一行代码即可生效

---
+++ markdeep-slides/slides-init.js
@@ -0,0 +1,33 @@
+document.write(`
+<!-- Markdeep slides stuff -->
+<script>
+markdeepSlidesOptions= {
+    aspectRatio: 16 / 10,
+    theme: "markdeep-slides/themes/simple.css",
+    fontSize: 28,
+    diagramZoom: 1.0,
+    totalSlideNumber: true,
+    progressBar: true,
+    breakOnHeadings: true,
+    incrementalFlat: false,
+    slideChangeHook: (oldSlide, newSlide) => {},
+    modeChangeHook: (newMode) => {}
+};
+</script>
+<link rel="stylesheet" href="markdeep-slides/lib/markdeep-relative-sizes/1.11/relativize.css">
+<link rel="stylesheet" href="markdeep-slides/markdeep-slides.css">
+<script src="markdeep-slides/markdeep-slides.js"></script>
+
+<script>
+    markdeepOptions = {
+        tocStyle: 'none',
+        detectMath: false,
+        onLoad: function() {
+            initSlides();
+        }
+    };
+</script>
+<style class="fallback">body{visibility:hidden;white-space:pre;font-family:monospace}</style>
+<script src="markdeep-slides/lib/markdeep/1.11/markdeep.min.js" charset="utf-8"></script>
+<script>window.alreadyProcessedMarkdeep||(document.body.style.visibility="visible")</script>
+`);
