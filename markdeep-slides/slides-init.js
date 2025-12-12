6ec220c8d7334b75bd4062a8b728494b29df6c5a
Wenbo Peng <pengwenbogood@126.com>
2025-12-12 10:48:04 +0800

    bug修复： 播放模式下目录内容位置不固定，特别是4:3比例的幻灯片

--- markdeep-slides/slides-init.js
+++ markdeep-slides/slides-init.js
@@ -2,7 +2,7 @@ document.write(`
 <!-- Markdeep slides stuff -->
 <script>
 markdeepSlidesOptions= {
-    aspectRatio: 16 / 10,
+    aspectRatio: 16 / 9,
     theme: "markdeep-slides/themes/simple.css",
     fontSize: 28,
     diagramZoom: 1.0,
